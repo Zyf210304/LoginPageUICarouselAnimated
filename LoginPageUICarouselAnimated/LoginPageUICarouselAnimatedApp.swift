ecb97f366fda0905218c3c9c0184adfdced09148
张亚飞 <672233580@qq.com>
2021-06-14 09:26:14 +0800

    Initial Commit

---
+++ LoginPageUICarouselAnimated/LoginPageUICarouselAnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageUICarouselAnimatedApp.swift
+//  LoginPageUICarouselAnimated
+//
+//  Created by 张亚飞 on 2021/6/14.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageUICarouselAnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
