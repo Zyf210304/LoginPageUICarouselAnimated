6a836a601fbd30c4103e2385a260e440c27813d6
张亚飞 <672233580@qq.com>
2021-06-14 13:45:11 +0800

    飞

---
+++ LoginPageUICarouselAnimated/View/Home.swift
@@ -0,0 +1,151 @@
+//
+//  Home.swift
+//  LoginPageUICarouselAnimated
+//
+//  Created by 张亚飞 on 2021/6/14.
+//
+
+import SwiftUI
+
+struct Home: View {
+    
+    @State var maxCircleheight: CGFloat = 0
+    @State var showSignUp = false
+    
+    var body: some View {
+        
+        VStack {
+            
+            //top rings view ...
+            //max height will be width of the screen
+            
+            // why geometry reader?
+            //since height will vary for different screens...
+            //so in order to get the height we used....
+            GeometryReader { proxy -> AnyView in
+                
+                let height = proxy.frame(in: .global).height
+                
+                DispatchQueue.main.async {
+                    
+                    if maxCircleheight == 0 {
+                        
+                        maxCircleheight = height
+                    }
+                }
+                
+                return AnyView(
+                
+                    ZStack {
+                        
+                        Circle()
+                            .fill(Color("dark"))
+                            .offset(x: getRect().width / 2, y: -height / 1.3)
+                        
+                        Circle()
+                            .fill(Color("dark"))
+                            .offset(x: -getRect().width / 2, y: -height / 1.5)
+                    
+                        Circle()
+                            .fill(Color("lightBlue"))
+                            .offset(y: -height / 1.3)
+                            .rotationEffect(.init(degrees: -5))
+                    
+                    
+                    }
+                )
+            }
+            .frame(maxHeight: getRect().width)
+            
+            
+            ZStack {
+                
+                // transitions...
+                if showSignUp {
+                    
+                    SignUp()
+                        .transition(.move(edge: .trailing))
+                }
+                else {
+                    
+                    Login()
+                        .transition(.move(edge: .trailing))
+                }
+                
+            }
+            //removing unwanted space...
+            .padding(.top, -maxCircleheight / 1.6)
+            .frame(maxHeight: .infinity, alignment: .top)
+            
+        }
+        .overlay(
+        
+            HStack {
+            
+            Text(showSignUp ?  "New Member" : "Already Member")
+                    .fontWeight(.bold)
+                    .foregroundColor(.gray)
+            
+                Button {
+                    
+                    withAnimation {
+                        
+                        showSignUp.toggle()
+                    }
+                    
+                } label: {
+                    
+                    Text(showSignUp ? "sign in" : "sign up")
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("lightBlue"))
+                }
+
+            }
+            .padding(.bottom, getSafeArea().bottom == 0 ? 15 : 0)
+            
+            ,alignment: .bottom
+        )
+        .background(
+        
+            //bottom rings...
+            HStack {
+                
+                Circle()
+                    .fill(Color("lightBlue"))
+                    .frame(width: 80, height: 80)
+                    .offset(x: -30, y: getRect().height < 750 ? 10 : 0)
+                Spacer()
+                
+                Circle()
+                    .fill(Color("dark"))
+                    .frame(width: 110, height: 110)
+                    .offset(x: 40, y: 20)
+                    
+            }
+            .offset(y: getSafeArea().bottom + 30)
+            
+            ,alignment: .bottom
+        )
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
+
+
+extension View {
+    
+    func getRect() -> CGRect {
+        
+        return UIScreen.main.bounds
+        
+    }
+    
+    func getSafeArea() -> UIEdgeInsets {
+        
+        return UIApplication.shared.windows.first?.safeAreaInsets ?? UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+}
